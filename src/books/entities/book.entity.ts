1baf21b99a129f51603245b38f9ed98c2d44eacd
Pedro <pedroball0120@gmail.com>
2024-01-25 17:10:20 -0400

    Crear modulo de book  y configurar la entity de book

---
+++ src/books/entities/book.entity.ts
@@ -0,0 +1,30 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Book {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column('text',{
+        unique:true,
+    })
+    title:string;
+    
+    @Column('text')
+    description:string;
+    
+    @Column('text')
+    gender:string;
+    
+    @Column('text')
+    author:string;
+
+    @Column('text')
+    authorPopularity:string;
+    
+    @Column('text')
+    bookPopularity:string;
+
+    @Column()
+    yearOfPublication:number;
+}
